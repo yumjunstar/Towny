94212fd53aab6494b71084f5a181a661f0622dfb
Llm Dl <LlmDlio@gmail.com>
2020-11-02 16:34:50 -0600

    War Event Fun Times Commit.
    - Make it so the new playerkill listener probably actually works.

--- src/com/palmergames/bukkit/towny/war/eventwar/EventWarListener.java
+++ src/com/palmergames/bukkit/towny/war/eventwar/EventWarListener.java
@@ -1,6 +1,5 @@
 package com.palmergames.bukkit.towny.war.eventwar;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
@@ -40,57 +39,70 @@ public class EventWarListener implements Listener {
 	
 	@EventHandler
 	private void onPlayerKillsPlayer(PlayerKilledPlayerEvent event) {
-		Player killer = event.getKiller();
-		Player victim = event.getVictim();
 		Resident killerRes = event.getKillerRes();
 		Resident victimRes = event.getVictimRes();
-		War killerWar = TownyUniverse.getInstance().getWarEvent(killer);
-		War victimWar = TownyUniverse.getInstance().getWarEvent(victim);
+		War killerWar = TownyUniverse.getInstance().getWarEvent(event.getKiller());
+		War victimWar = TownyUniverse.getInstance().getWarEvent(event.getVictim());
 
 		if (killerWar == null || victimWar == null)
 			return; // One of the players is not in a war.
 		if (killerWar != victimWar)
 			return; // The wars are not the same war.
-		if (CombatUtil.isAlly(killer.getName(), victim.getName()))
+		if (CombatUtil.isAlly(killerRes.getName(), victimRes.getName()))
 			return; // They are allies and this was a friendly fire kill.
 
-		victimWar.takeLife(victimRes);
-		
-		
+		// TODO: Potentially redo removal code to only accept residents and calculate Towns and Nations from them/Not throw NotRegisteredExceptions.
 		Town killerTown = null;
 		try {
 			killerTown = killerRes.getTown();
 		} catch (NotRegisteredException ignored) {}
 		
-		switch (killerWar.getWarType()) {
-		case RIOT:
-			break;
-		case NATIONWAR:
-		case WORLDWAR:
-			if (killerWar.getLives(victimRes) == 0 && killerTown != null && TownySettings.isRemovingOnMonarchDeath()) {
-				try {
-					if (victimRes.hasNation() && victimRes.isKing()) {
-						TownyMessaging.sendGlobalMessage(Translation.of("MSG_WAR_KING_KILLED", victimRes.getTown().getNation().getName()));
-						killerWar.remove(killerTown, victimRes.getTown().getNation()); // Remove the king's nation from the war.
-					}
-				} catch (NotRegisteredException ignored) {}
-			}
-			break;
-			
-		case CIVILWAR:
-		case TOWNWAR:
-			if (killerWar.getLives(victimRes) == 0 && killerTown != null && TownySettings.isRemovingOnMonarchDeath()) {
-				try {
-					if (victimRes.hasTown() && victimRes.isMayor()) {
-						TownyMessaging.sendGlobalMessage(Translation.of("MSG_WAR_MAYOR_KILLED", victimRes.getTown().getName()));
-						killerWar.remove(killerTown, victimRes.getTown()); // Remove the mayor's town from the war.
-					}
-				} catch (NotRegisteredException ignored) {}
-			}
-			break;
+		int victimLives = killerWar.getLives(victimRes); // Use a variable for this because it will be lost once takeLife(victimRes) is called.
+		/*
+		 * Take a life off of the victim no matter what type of war it is.
+		 */
+		victimWar.takeLife(victimRes);
 		
+		/*
+		 * Handle king/mayor deaths, which can potentially remove them from the war if they have no more lives.
+		 */
+		switch (killerWar.getWarType()) {
+		
+			case RIOT:
+				break;
+			case NATIONWAR:
+			case WORLDWAR:
+				/*
+				 * Look to see if the king's death would remove the nation from the war.
+				 */
+				if (victimLives == 0 && TownySettings.isRemovingOnMonarchDeath()) {
+					try {
+						if (victimRes.hasNation() && victimRes.isKing()) {
+							TownyMessaging.sendGlobalMessage(Translation.of("MSG_WAR_KING_KILLED", victimRes.getTown().getNation().getName()));
+							killerWar.remove(killerTown, victimRes.getTown().getNation()); // Remove the king's nation from the war.
+						}
+					} catch (NotRegisteredException ignored) {}
+				}
+				break;
+			case CIVILWAR:
+			case TOWNWAR:
+				/*
+				 * Look to see if the mayor's death would remove the town from the war.
+				 */
+				if (victimLives == 0 && TownySettings.isRemovingOnMonarchDeath()) {
+					try {
+						if (victimRes.hasTown() && victimRes.isMayor()) {
+							TownyMessaging.sendGlobalMessage(Translation.of("MSG_WAR_MAYOR_KILLED", victimRes.getTown().getName()));
+							killerWar.remove(killerTown, victimRes.getTown()); // Remove the mayor's town from the war.
+						}
+					} catch (NotRegisteredException ignored) {}
+				}
+				break;
 		}	
 
+		/*
+		 * Give the killer some points.
+		 */
 		if (TownySettings.getWarPointsForKill() > 0){
 			killerWar.residentScoredKillPoints(victimRes, killerRes, event.getLocation());
 		}
